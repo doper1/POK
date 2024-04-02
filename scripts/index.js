24eb6436bea6da1afb3a79e31bf0b1824021c15a
Adam Ofer <adamofer42@gmail.com>
2024-04-02 21:08:31 +0300

    E&A merge

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,7 @@ let Player = require('./player');
 let constants = require('./constants')
 let general_functions = require('./general_functions')
 let poker_functions = require('./poker_functions');
-//const Player = require('player');
-
+let Game = require('./Game');
 
 let whatsapp = new Client({
     authStrategy:new LocalAuth()
@@ -17,68 +16,87 @@ whatsapp.on('qr', qr=> {
     })
 })
   
-// Variables
-let pot = 0;
-let players = []
 
-whatsapp.on('message', async message => {
+let games = {};
+whatsapp.on('message', async msg => {
+    let message = await msg;
     let chat = await message.getChat();
-    let chatName = chat.name || 'Unknown Group';
-    let playerName = (await message.getContact()).pushname;
-    let playerNumber = (await message.author);
-
-    console.log(`Message received in group: ${ general_functions.reverseString(chatName)}`);
-
-    if (chatName.includes("נבחרתם שבוע הבא") || chatName.includes("סלפי אחד") || chatName.includes("קלף חינם")){
+    let contact = await message.getContact();
+    let chat_id = chat.id._serialized.replace(/\D/g, '');
+    
+    if (chat.name.includes("נבחרתם שבוע הבא") || chat.name.includes("טסטים פוקר") || chat.name.includes("קלף חינם")){
         let user_msg = message.body.toLowerCase();
 
         // poker usage - learn about the functions
-        if (user_msg === "poker usage" || user_msg === "poker help" || user_msg === "poker ?" || user_msg === "poker") {
-            message.reply('poker- usage, join, show, exit, start, finish, check, raise [amount], fold');
+        if (user_msg === "poker usage" || user_msg === "poker help") {
+            message.reply('poker- usage, create, join, show, exit, start, finish, check, raise [amount], fold');
         }
-        
+        if (user_msg === "poker check"){
+            let player = new Player(playerName, playerNumber);
+            player.setHoleCards(['5','Diamond'],['Your','Mother'])
+            whatsapp.sendMessage(player.getPhoneNumber(),`Hole cards: ${player.getHoleCards()}`);
+        }
+
         // poker join (the game)
-        if (user_msg === "poker join") {
+        if (user_msg === "poker join") 
+        {
+            if (!(chat_id in games)){
+                games[chat_id] = new Game(chat_id,null,0,"r");
+                games[chat_id].addPlayer(contact.pushname, message.author);
+                message.reply( `${(contact.pushname).split(" ")[0]} have joined the game!`);
+            }   
+            else {
+                let found = false;
+                for (let i = 0; i < games[chat_id].players.length; i++){
+                    if (message.author === games[chat_id].players[i].phone_number){
+                        found = true;
+                        break;
+                    }
+                }
 
-            if (!(playerName in players)) {
-                const player = new Player(playerName, playerNumber)
-                players.push(player);
-                message.reply(`${player.getName()} joined the Table`);
-
-            } else {
-                message.reply(`${playerName} already joined the Table`);
-            }
+                if (found)
+                    message.reply('you have already joined!');
+                else{
+                    games[chat_id].addPlayer(message.author.id,message.getContact().pushname,message.author);
+                    message.reply( "you have joined the game!");}
+                }   
         }
+
 
         // poker show (players)
         if (user_msg === "poker show") {
-            message.reply(`Players: ${Object.keys(players).join(', ')}`);
+            let players_list = ""
+            let players = games[chat_id].players;
+            
+            if(!(chat_id in games)){
+                message.reply("There are no players on the table :(");
+            }
+            else {
+                for( let i = 0; i < players.length; i++){
+                    players_list += `${i+1}. ${players[i].name}\n`
+
+                message.reply(`Players:\n ${players_list}`);
+                }
+            }
         }
+
 
         // poker exit (the table)
         if (user_msg === "poker exit") {
-    
-            if (players.hasOwnProperty(player.getName())) { // Switch it a a Loop on all the player names
-                delete players[playerName];
-                message.reply(`${playerName} left the Table`);
-            } else {
-                message.reply(`${playerName} is not on the Table`);
+            for (let i = 0; i < games[chat_id].players.length; i++) {
+            message.reply(games[chat_id].players[i].name + " money is " + games[chat_id].players[i]);
             }
+            games[chat_id] = undefined;
         }
 
         // poker start - start the game
         if (user_msg === "poker start"){
-            let order = general_functions.shuffleArray(players);
-            let deck = general_functions.createDeck();
-            deck = general_functions.shuffleArray(deck);
+            let order = general_functions.shuffleArray(games[chat_id].getPlayers());
             console.log(`Order: ${order}\nDeck: ${deck}`)
-            //message.sendMessage() // shuffleArray(deck);
-            //message.reply(`Playing Order:\n${players.map((item, index) => `${index + 1}. ${item}`).join(' ')}`);
-            //message.reply(`1. ${deck.pop()}\n2. ${deck.pop()}\n3. ${deck.pop()}\n`)
-            //message.sendMessage("TEST MESSAGE")
         }
     }
 })
+
 whatsapp.on('ready',() => {
     console.log("Client is ready!")
 })
