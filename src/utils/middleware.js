c0e36c35ad853949fe33d30a0185adb3f64bf370
adamofer42 <adamofer42@gmail.com>
2024-10-30 20:47:53 +0200

    feat(middleware): LLM transaltion POC

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -2,6 +2,7 @@ const constants = require('./constants');
 const { getProperties, currentTime } = require('./generalFunctions');
 const Mustache = require('mustache');
 const Game = require('../models/Game.js');
+const Groq = require('groq-sdk');
 
 function logMessage(message, chatName, messageLevel) {
   const template = `CHAT: {{chatName}} || FROM: {{author}} || MESSAGE: {{body}}`;
@@ -78,6 +79,44 @@ async function getGame(chat) {
   return game;
 }
 
+const groq = new Groq({ apiKey: process.env.Groq_API_KEY });
+
+async function translate(body) {
+  return groq.chat.completions.create({
+    messages: [
+      {
+        role: 'system',
+        content: ` 
+The message you will receive is poker related.
+Your goal is to translate it to a command.
+
+The possible commands are:
+pok check
+pok call
+pok raise [amount]
+pok all (in)
+pok fold
+pok buy [amount] 
+pok help 
+pok join [amount]
+pok show
+pok exit
+pok end
+
+Your goal is to translate each message you receive to an accurate command.
+If you think the message is unrelated and should not be translated, reply with "not related"
+`,
+      },
+      {
+        role: 'user',
+        content: `${body} `,
+      },
+    ],
+
+    model: 'llama-3.1-8b-instant',
+  });
+}
+
 module.exports = {
   validateMessage,
   filterWhatsapp,
@@ -88,4 +127,5 @@ module.exports = {
   lockGame,
   unlockGame,
   getGame,
+  translate,
 };
