4eb4adfb77c46d9ef1294fbe0b46756b646f36b8
adamofer42 <adamofer42@gmail.com>
2025-01-24 19:32:33 +0200

    fix: optipng run time

--- imagen/src/image.rs
+++ imagen/src/image.rs
@@ -9,10 +9,10 @@ async fn generate_image(
     bin: &str,
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let args_1: [&str; 10] = [
-        "-border", "300", "-resize", "50%", "-filter", "Lanczos", "-quality", "1", "+append", "-",
+        "-border", "30", "-resize", "50%", "-filter", "Lanczos", "-quality", "100", "+append", "-",
     ];
 
-    let args_2: [&str; 5] = ["-", "-border", "150x150", "-strip", target];
+    let args_2: [&str; 5] = ["-", "-border", "15x15", "-strip", target];
 
     let aggregate = Command::new(bin)
         .args(cards)
